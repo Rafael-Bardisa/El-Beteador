aa4056b547323fb84aace36bd1d0d6c0348c5554
Rafael-Bardisa <rafael.bardisa01@estudiant.upf.edu>
2021-11-11 22:00:22 +0100

    safari driver

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-#test
+# test
+from selenium import webdriver
 def main():
     print('hello, world!')
+    print(str(z(3, 1)))
+    browser = webdriver.Safari()
+    browser.get("https://google.com")
+    end = 0
+    input(end)
+
+
+# comprueba si vale la pena apostar si hay cuotas a, b
+def z(a, b):
+    return a*b - (a+b)
 
 main()
