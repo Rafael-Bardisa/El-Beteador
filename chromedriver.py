cb84bb687541149f4441bba8fcdfa6ef765549f4
ricardopq <94146433+ricardopq@users.noreply.github.com>
2021-11-13 14:36:47 +0100

    Create chromedriver.py

---
+++ chromedriver.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import pandas
+import time
+
+driver = webdriver.Chrome("C:/Users/Usuario/Desktop/Bets/chromedriver")
+bet365 = "https://www.bet365.es/#/OF/"
+codere = "https://www.codere.es/"
+
+#Entra en Bet365
+driver.get(bet365)
+
+#Espera y entra en tenis
+time.sleep(1)
+driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div/div[29]").click()
+
+#Espera y entra en la lista de partidos
+time.sleep(1)
+driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[2]/div/div/div[2]/div[1]/span").click()
+
+
+databet365 = pandas.DataFrame()
+
