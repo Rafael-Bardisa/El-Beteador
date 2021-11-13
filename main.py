5c6832d9b16deaa0819c67270457399b9bccc4a1
Rafael-Bardisa <rafael.bardisa01@estudiant.upf.edu>
2021-11-13 21:43:35 +0100

    un sec

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # test
+import numpy as np
 from selenium import webdriver
 def main():
     print('hello, world!')
     print(str(z(3, 1)))
     browser = webdriver.Safari()
-    browser.get("https://google.com")
+    browser.get("https://bet365.es")
     end = 0
     input(end)
+
 
 
 # comprueba si vale la pena apostar si hay cuotas a, b
