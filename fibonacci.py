f64eb3c233c3cb8038002e2f0193a76b146c1f90
Rafael-Bardisa <rafael.bardisa01@estudiant.upf.edu>
2022-01-15 14:17:07 +0100

    borrado todos que ya estan

--- fibonacci.py
+++
@@ -1,5 +0,0 @@
-def fibo(n):
-    return 1 if n in [1, 2] else (fibo(n - 1) + fibo(n - 2))
-
-if __name__ == '__main__':
-    print(dir())
--- main.py
+++ main.py
@@ -3,37 +3,25 @@ import time
 
 import pandas as pd
 # import sys
-# TODO hacer un paquete chido para que todo se importe solo
 # mágicamente coge los archivos del subdirectorio, pycharm no detecta que se use pero es mentira
 from scrap_modules import *
-
 
 red = '\33[91m'
 yellow = '\33[93m'
 blue = '\33[94m'
 reset = '\33[0m'
 
-
 # encuentra todos los scrappers y los pone en un diccionario {modulo: url}
 dictardo = {mod: mod.url for ref, mod in globals().items() if '_scrap' in ref}
-#diccionario vacio para guardar los modulos filtrados
+# diccionario vacio para guardar los modulos filtrados
 modulos = {}
-# TODO que casas usamos
+# que casas usamos
 nombre_casas = []
+
 
 # quita el _scrap del nombre de los archivos
 def format_name(string):
     return string.split('.')[1].split('_')[0]
-
-
-# para testeo, ignorable
-if __name__ == '__main__':
-    print(f'{dir()}\n{globals()}')
-    droplist = input(
-        f'Modulos importados: {[format_name(mod.__name__) for mod in dictardo]}\ndroplist: (space separated):').split()
-    modulos.update({key: val for key, val in dictardo.items() if format_name(key.__name__) not in droplist})
-    nombre_casas[:] = [format_name(mod.__name__) for mod in modulos]
-    print(f'{modulos}\n{nombre_casas}\n{dictardo}')
 
 
 # inicia ventanas automaticamente para que los handles esten en orden:
@@ -45,7 +33,7 @@ def init_browser(driver):
 
 
 # comprueba si vale la pena apostar si hay cuotas a, b
-def z(a, b):
+def z(a, b) -> float:
     return a * b - (a + b)
 
 
@@ -62,13 +50,12 @@ def split_cuotas(dict_cuotas):
 
 
 # dataframe de una sola cuota, hay que pasarle listado de nombres de casas en el orden que las scrapeamos
-def build_dataframe(casas_cuota):
+def build_dataframe(casas_cuota) -> pd.DataFrame:
     cuota_frame = pd.DataFrame()
     for idx, casa in enumerate(casas_cuota):  # por cada data de cada casa
         series = pd.Series(casa)  # convierte la data a series
 
         # chapuza para coger el numbre de la casa
-        # TODO arreglar la chapuza para que sea mas automatico
         nombre = nombre_casas[idx]
 
         # añade al dataframe, hay que hacer reassign because pandas
@@ -78,7 +65,7 @@ def build_dataframe(casas_cuota):
 
 
 # une los maximos e indices en un dataframe para tener la info ordenada
-def big_merge(cuota_1, cuota_2, casa_1, casa_2):
+def big_merge(cuota_1, cuota_2, casa_1, casa_2) -> pd.DataFrame:
     cuota_summary = pd.concat([cuota_1, cuota_2], axis=1)
     casa_summary = pd.concat([casa_1, casa_2], axis=1)
     data_summary = cuota_summary.merge(casa_summary, left_index=True, right_index=True, how='outer')
@@ -86,21 +73,15 @@ def big_merge(cuota_1, cuota_2, casa_1, casa_2):
     return data_summary
 
 
-def big_scrap(driver):
-    casas = []  # lista vacia donde guardar los datos de las casas
+# scrapea las paginas como todo un chad
+def big_scrap(driver) -> pd.DataFrame:
+    # lista vacia donde guardar los datos de las casas
+    casas = []
     # listas para guardar dicts de {partido: una cuota}
     casas_cuota_1 = []
     casas_cuota_2 = []
 
-    # TODO scrapea las paginas
-    # asume data: {partido: [cuota 1, cuota 2]}
-
-    # TODO quitar el range, necesita testear
-    # for i in range(len(URLs)):
-    #    driver.switch_to.window(driver.window_handles[i])
-    #    data = modulos[i].scrap(driver)
-    #    casas.append(data)
-
+    # scrapea las paginas usando los modulos
     for idx, modulo in enumerate(modulos):
         driver.switch_to.window(driver.window_handles[idx])
         data = modulo.scrap(driver)
@@ -130,12 +111,13 @@ def big_scrap(driver):
 
 
 def BETI(driver):
-    droplist = input(
+    beti_list = input(
         f'Modulos importados: {[format_name(mod.__name__) for mod in dictardo]}\ndroplist: (space separated):').split()
-    modulos.update({mod: mod_url for mod, mod_url in dictardo.items() if format_name(mod.__name__) not in droplist})
+    modulos.update({mod: mod_url for mod, mod_url in dictardo.items() if format_name(mod.__name__) not in beti_list})
     nombre_casas[:] = [format_name(mod.__name__) for mod in modulos]
 
-    print(f'\nScrapeadores activos: {blue}{nombre_casas}\n{red}ELIMINA LOS DOBLES{reset}\nURLs por orden:\n{str(list(modulos.values()))}')
+    print(
+        f'\nScrapeadores activos: {blue}{nombre_casas}\n{red}ELIMINA LOS DOBLES{reset}\nURLs por orden:\n{str(list(modulos.values()))}')
 
     init_browser(driver)  # abre las paginas en orden
     dineros = int(input(f'{yellow}Enter bet cuando las pestañas:{reset} '))
@@ -148,3 +130,12 @@ def BETI(driver):
 
         # volver a ejecutar loop o salir (<0)
         dineros = int(input(f'{yellow}nueva bet:{reset} '))
+
+
+if __name__ == '__main__':
+    print(f'{dir()}\n{globals()}')
+    droplist = input(
+        f'Modulos importados: {[format_name(mod.__name__) for mod in dictardo]}\ndroplist: (space separated):').split()
+    modulos.update({key: val for key, val in dictardo.items() if format_name(key.__name__) not in droplist})
+    nombre_casas[:] = [format_name(mod.__name__) for mod in modulos]
+    print(f'{modulos}\n{nombre_casas}\n{dictardo}')
