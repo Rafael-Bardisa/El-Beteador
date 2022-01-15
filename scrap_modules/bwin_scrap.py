684b49a2b2989f5fa7ddba8e9c9eb7da2ff5d89e
Rafael-Bardisa <rafael.bardisa01@estudiant.upf.edu>
2022-01-15 20:53:11 +0100

    desmontado driver en mas funciones para mejor readability

--- scrap_modules/bwin_scrap.py
+++ scrap_modules/bwin_scrap.py
@@ -8,6 +8,37 @@ url = "https://sports.bwin.es/es/sports/tenis-5/apuestas"
 # driv = webdriver.Chrome("/Users/rafaelbardisarodes/Desktop/beteador/chromedriver", chrome_options=chromedriver.camo())
 
 
+def extract_matches(bwin_matches):
+    # Extrae cuotas y nombres
+    bwin_cuotas = []
+    bwin_names = []
+    for bwin_match in bwin_matches:
+        match = bwin_match.text.split('\n')
+        if '/' not in match[0]:
+            bwin_cuotas.append(match[(len(match) - 2)])
+            bwin_cuotas.append(match[(len(match) - 1)])
+            bwin_names.append(match[0])
+            bwin_names.append(match[1])
+    return bwin_cuotas, bwin_names
+
+
+def format_name(elem):
+    """
+    devuelve el nombre de un tenista en formato "apellido inicial"
+    tambien quita los tags de los paises
+    :param elem: nombre del tenista
+    :return: f'{surname}, {name[0]}
+    """
+    fullname = elem.split()
+    name_size = len(fullname)
+    name = fullname[0][0]
+    if name_size == 2:
+        surname = fullname[1][0:-3]
+    else:
+        surname = fullname[1]
+    return f'{surname} {name}'
+
+
 # TODO mas tests para asegurarse de que es robusto
 def scrap(driver) -> dict:
     """
@@ -16,63 +47,20 @@ def scrap(driver) -> dict:
     :return william_dict: diccionario estilo {match: [cuota 1, cuota 2]
     """
     bwin_matches = driver.find_elements(By.CLASS_NAME, "grid-event-wrapper")
-    # bwinmatch[0].text
 
-    # Extrae cuotas y nombres
-    bwin_cuotas = []
-    bwin_names = []
-    for i in range(len(bwin_matches) * 2):
-        match = bwin_matches[i // 2].text.split('\n')
-        if '/' not in match[0]:
-            if i % 2 == 0:
-                bwin_cuotas.append(match[(len(match) - 2)])
-                bwin_names.append(match[0])
-            else:
-                bwin_cuotas.append(match[(len(match) - 1)])
-                bwin_names.append(match[1])
-
-    for bwin_match in bwin_matches:
-        match = bwin_match.text.split('\n')
-        if '/' not in match[0]:
-            bwin_cuotas.append(match[(len(match) - 2)])
-            bwin_cuotas.append(match[(len(match) - 1)])
-            bwin_names.append(match[0])
-            bwin_names.append(match[1])
-
-    # for i in range(len(bwin_cuotas)):
-    #    convierte los elementos de las cuotas a numeros
-    #    bwin_cuotas[i] = pandas.to_numeric(bwin_cuotas[i])
+    # extrae la info de las matches
+    bwin_cuotas, bwin_names = extract_matches(bwin_matches)
 
     bwin_cuotas[:] = [pandas.to_numeric(cuota) for cuota in bwin_cuotas]
+    # formatea los nombres correctamente
+    true_bwin_names = [format_name(elem) for elem in bwin_names]
+    # empareja los rivales
+    truer_bwin_names = [f'{local} {visitor}' for local, visitor in zip(true_bwin_names[::2], true_bwin_names[1::2])]
 
-    # print(bwincuotas)
-    # print(bwinnames)
-    # print(len(bwincuotas))
-    true_bwin_names = []
-    for elem in bwin_names:
-        fullname = elem.split()
-        name_size = len(fullname)
-
-        name = fullname[0][0]
-        if name_size == 2:
-            surname = fullname[1][0:-3]
-        else:
-            surname = fullname[1]
-        true_bwin_names.append(f'{surname} {name}')
-
-    truer_bwin_names = []
-    for local, visitor in zip(true_bwin_names[::2], true_bwin_names[1::2]):
-        truer_bwin_names.append(f'{local} {visitor}')
-
-    # print(truerbwinnames)
-    # print(len(truerbwinnames))
     # crea el diccionario magico que usa el main para crear la dataframe final
-    bwin_dict = {}
-    # for i in range(len(truer_bwin_names)):
-    #    bwin_dict[truer_bwin_names[i]] = [bwin_cuotas[i * 2], bwin_cuotas[(i * 2) + 1]]
+    bwin_dict = {name: cuotas for name, cuotas in
+                 zip(truer_bwin_names, map(list, zip(bwin_cuotas[::2], bwin_cuotas[1::2])))}
 
-    for name, cuotas in zip(truer_bwin_names, map(list, zip(bwin_cuotas[::2], bwin_cuotas[1::2]))):
-        bwin_dict[name] = cuotas
     return bwin_dict
 
 
