029eac73f120e390b02cdf7a404ec1c7f8c5819c
ricardopq <94146433+ricardopq@users.noreply.github.com>
2022-01-16 21:15:18 +0100

    Create betfred_scrap.py
    creado automaticamente *carita fachera*

---
+++ scrap_modules/betfred_scrap.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas
+
+
+# url de la pagina:
+url = 'https://www.betfred.es/ES/512/sports#bo-navigation=356554.1&action=market-group-list'
+
+
+
+def split_match_names(names):
+    
+    split_names = []
+    for elem in names:
+        names1 = elem.split(' v ')
+        for i in names1:
+            surnames, name = i.split(', ')
+            surname = surnames.split()[0]
+            name = name[0]
+            split_names.append(f'{surname} {name}')
+        
+    return split_names
+
+
+
+
+def scrap(driver) -> dict:
+    
+    betfreddata = driver.find_elements_by_tag_name("td")
+    for i in range(len(betfreddata)):    
+        betfreddata.append(betfreddata[i].text)
+    
+    
+    data_filter = [dataelem for dataelem in betfreddata if not (dataelem == '' or dataelem[0] in ['-','+']) ]
+
+    
+    cuota1 = map(pandas.to_numeric, data_filter[2::4])
+    cuota2 = map(pandas.to_numeric, data_filter[3::4])
+
+    betfredrawnames = data_filter[1::4]
+    betfred_dict = {}    
+    
+    betfrednames = split_match_names(betfredrawnames)
+    
+    betfred_dict = {name: cuotas for name, cuotas in
+                    zip(betfrednames, map(list, zip(cuota1, cuota2)))}
+     
+    return betfred_dict
+
+
+def main(): # de testeo para comprobar que la funcion va bien
+	import chromedriver
+	
+	driver = webdriver.Chrome("/Users/rafaelbardisarodes/Desktop/beteador/chromedriver",
+								chrome_options=chromedriver.camo())
+	input(f'{url = !s}')
+	print(scrap(driver))
+	input('exit')
+
+
+if __name__ == '__main__':  # testea solo el scrapper de william
+	main()
