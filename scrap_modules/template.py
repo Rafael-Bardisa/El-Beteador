2756abb54ebf7826e2fb61306f43b0941510212f
Rafael-Bardisa <rafael.bardisa01@estudiant.upf.edu>
2022-01-19 00:44:08 +0100

    write_template acepta nombres estilo 'foo_scrap.py'

--- scrap_modules/template.py
+++ scrap_modules/template.py
@@ -11,9 +11,10 @@ def write_template(name):  # xd moment
         docstring = f'\t"""\n\tScrapea la pagina {name} y recoge las cuotas de los partidos de tenis\n\t:param driver: referencia a un driver de selenium\n\t:return william_dict: diccionario estilo {formato}\n\t"""'
         j_map = '{return match.innerText}'
         jScript = f'jScript = """const {name}matches = Array.prototype.slice.call(document.getElementsBy########("########"))\nreturn {name}matches.map(function (match){j_map})"""\n\t'
+        scrap_data = f'{name}_data = driver.execute_script(jScript)\n\t'
         nuldict = '{}'
         scrap = 'def scrap(driver) -> dict:\n'
-        func = f'{scrap}{docstring}\n\t# replacea los ######## por lo que usarias con el driver find elements\n\t{jScript}\n\t# diccionario a llenar con las datas scrapeadas\n\t{name}_dict = {nuldict}\n\n\treturn {name}_dict\n'
+        func = f'{scrap}{docstring}\n\t# replacea los ######## por lo que usarias con el driver find elements\n\t{jScript}\n\t{scrap_data}\n\t# diccionario a llenar con las datas scrapeadas\n\t{name}_dict = {nuldict}\n\n\treturn {name}_dict\n'
         furl = "f'{url = !s}'"
         print_statement = "f'{key}: {val}'"
         print_dict = f"def print_dict(dict):\n\tfor key, val in dict.items():\n\t\tprint({print_statement})\n"
@@ -29,9 +30,11 @@ def main():
     blue = '\33[94m'
 
 
-    new_files = input(f'Enter module names (space separated)\nExample name: {blue}[william]{reset}_scrap.py\n\nModule: ').split()
+    new_files = input(f'Enter module names (space separated)\nExample name: {blue}william{reset}_scrap.py\n\nModule: ').split()
     for new_file in new_files:
         try:
+            if '_scrap.py' in new_file:  # permite escribir tambien la extension
+                new_file = new_file.split('_')[0]
             write_template(new_file)
         except FileExistsError:  # pilla que la funcion ha petado y te lo dice en rojo
             print(f'{red}{new_file}_scrap file already exists!{reset}')
